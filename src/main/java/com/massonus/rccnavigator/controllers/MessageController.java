0db6ad2355fd9b17c4108687d7e7b184d9c43e2d
Massonus <maksym.lithuanian@gmail.com>
2024-02-18 15:24:06 +0200

    refactor messages

--- src/main/java/com/massonus/rccnavigator/controllers/MessageController.java
+++ src/main/java/com/massonus/rccnavigator/controllers/MessageController.java
@@ -1,28 +1,10 @@
 package com.massonus.rccnavigator.controllers;
 
-import com.massonus.rccnavigator.entity.Message;
 import com.massonus.rccnavigator.entity.User;
 import com.massonus.rccnavigator.service.MessageService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/message")
@@ -35,8 +17,8 @@ public class MessageController {
     }
 
     @PostMapping("/new-product-message/{id}")
-    public String addNewProductMessage(@PathVariable Long id, @AuthenticationPrincipal User user, @RequestBody String text) {
-        messageService.saveMessage(user, id, text);
+    public String addNewProductMessage(@PathVariable Long id, @AuthenticationPrincipal User user, @RequestParam String comment) {
+        messageService.saveMessage(user, id, comment);
         return "redirect:/product/" + id;
     }
 
