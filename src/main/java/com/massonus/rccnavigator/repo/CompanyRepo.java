7d866f3d779f2c33543183c5bfaba57a2a6c0bf6
Massonus <maksym.lithuanian@gmail.com>
2024-02-19 20:10:36 +0200

    refactor

--- src/main/java/com/massonus/rccnavigator/repo/CompanyRepo.java
+++ src/main/java/com/massonus/rccnavigator/repo/CompanyRepo.java
@@ -2,10 +2,7 @@ package com.massonus.rccnavigator.repo;
 
 import com.massonus.rccnavigator.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
