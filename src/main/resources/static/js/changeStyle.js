0b9bca74fbed545f84fe6909d3cb6d0587a500be
Massonus <maksym.lithuanian@gmail.com>
2024-02-28 16:29:53 +0200

    save color checked

--- src/main/resources/static/js/changeStyle.js
+++ src/main/resources/static/js/changeStyle.js
@@ -1,21 +1,39 @@
 var theme = document.getElementById("theme");
+var nav = document.getElementById('navbar');
+
 
 function changeStyle() {
 
-    if (theme.getAttribute("href") === "/css/light-theme.css") {
-        // …то переключаемся на "dark-theme.css"
+    let checked = document.getElementById("checker").checked;
+
+    if (checked) {
+
         theme.href = "/css/dark-theme.css";
-        // Сохраняем значение в хранилище
+        nav.className = "navbar fixed-top navbar-expand-lg navbar-dark bg-dark";
         localStorage.setItem('theme', '/css/dark-theme.css');
-        // В противном случае…
+        localStorage.setItem('class', 'navbar fixed-top navbar-expand-lg navbar-dark bg-dark');
+        localStorage.setItem('checker', 'true');
     } else {
-        // …переключаемся на "light-theme.css"
+
         theme.href = "/css/light-theme.css";
-        // Сохраняем значение в хранилище
+        nav.className = "navbar fixed-top navbar-expand-lg navbar-light bg-light";
         localStorage.setItem('theme', '/css/light-theme.css');
+        localStorage.setItem('class', 'navbar fixed-top navbar-expand-lg navbar-light bg-light');
+        localStorage.setItem('checker', 'false');
     }
 }
 
 function setTheme() {
     theme.href = localStorage.getItem('theme') || '/css/light-theme.css';
+    nav.className = localStorage.getItem('class') || 'navbar fixed-top navbar-expand-lg navbar-light bg-light';
+
+    let checker = document.getElementById("checker");
+    let getItem = localStorage.getItem('checker') || false;
+    let isChecked = JSON.parse(getItem);
+
+    if (isChecked) {
+        checker.setAttribute("checked", "checked");
+    } else {
+        checker.removeAttribute("checked");
+    }
 }
