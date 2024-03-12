63a18bb31957dfc320f93abdc35e41d4bbfae5e0
Massonus <maksym.lithuanian@gmail.com>
2024-03-12 21:40:26 +0200

    changed dark theme styles

--- src/main/resources/static/js/changeStyle.js
+++ src/main/resources/static/js/changeStyle.js
@@ -3,6 +3,13 @@ var nav = document.getElementById('navbar');
 var sort = document.getElementById("sort");
 var kitchenCategory = document.getElementById("kitchenCategory");
 var companyCountry = document.getElementById("companyCountry");
+var cards = document.getElementsByClassName("card");
+var tableHeads = document.getElementsByClassName("thead");
+var formControls = document.getElementsByClassName("form-control");
+var userTable = document.getElementById("user-table");
+var categoryTable = document.getElementById("category-table");
+var companyTable = document.getElementById("company-table");
+var countryTable = document.getElementById("country-table");
 
 function changeStyle() {
 
@@ -12,6 +19,41 @@ function changeStyle() {
 
         theme.href = "/css/dark-theme.css";
         nav.className = "navbar fixed-top navbar-expand-lg navbar-dark bg-dark";
+
+        if (!(cards === null)) {
+            for (const element of cards) {
+                element.classList.remove("bg-light");
+                element.classList.add("bg-dark");
+            }
+        }
+
+        if (!(tableHeads === null)) {
+            for (const element of tableHeads) {
+                element.classList.remove("bg-light");
+                element.classList.add("bg-dark");
+            }
+        }
+
+        if (!(formControls === null)) {
+            for (const element of formControls) {
+                element.classList.remove("bg-light");
+                element.classList.add("bg-dark");
+            }
+        }
+
+        if (!(tableHeads === null)) {
+            for (const element of tableHeads) {
+                element.classList.remove("bg-light");
+                element.classList.add("bg-dark");
+            }
+        }
+
+        if (!(userTable === null)) {
+            userTable.className = "table bg-dark";
+            categoryTable.className = "table bg-dark";
+            countryTable.className = "table bg-dark";
+            companyTable.className = "table bg-dark";
+        }
 
         if (!(sort === null)) {
             sort.className = "form-select bg-dark";
@@ -25,6 +67,9 @@ function changeStyle() {
             companyCountry.className = "form-select bg-dark";
         }
 
+        localStorage.setItem('table', 'table bg-dark');
+        localStorage.setItem('old-card', 'bg-light');
+        localStorage.setItem('new-card', 'bg-dark');
         localStorage.setItem('sort', 'form-select bg-dark');
         localStorage.setItem('form-select', 'form-select bg-dark');
         localStorage.setItem('theme', '/css/dark-theme.css');
@@ -37,6 +82,33 @@ function changeStyle() {
         theme.href = "/css/light-theme.css";
         nav.className = "navbar fixed-top navbar-expand-lg navbar-light bg-light";
 
+        if (!(cards === null)) {
+            for (const element of cards) {
+                element.classList.remove("bg-dark");
+                element.classList.add("bg-light");
+            }
+        }
+
+        if (!(tableHeads === null)) {
+            for (const element of tableHeads) {
+                element.classList.remove("bg-dark");
+                element.classList.add("bg-light");
+            }
+        }
+
+        if (!(formControls === null)) {
+            for (const element of formControls) {
+                element.classList.remove("bg-dark");
+                element.classList.add("bg-light");
+            }
+        }
+
+        if (!(userTable === null)) {
+            userTable.className = "table table-striped table-hover";
+            categoryTable.className = "table table-striped table-hover";
+            countryTable.className = "table table-striped table-hover";
+            companyTable.className = "table table-striped table-hover";
+        }
 
         if (!(sort === null)) {
             sort.className = "form-select";
@@ -50,6 +122,9 @@ function changeStyle() {
             companyCountry.className = "form-select";
         }
 
+        localStorage.setItem('table', 'table table-striped table-hover');
+        localStorage.setItem('old-card', 'bg-dark');
+        localStorage.setItem('new-card', 'bg-light');
         localStorage.setItem('form-select', 'form-select');
         localStorage.setItem('sort', 'form-select');
         localStorage.setItem('theme', '/css/light-theme.css');
@@ -78,6 +153,34 @@ function setTheme() {
         checker.setAttribute("checked", "checked");
     } else {
         checker.removeAttribute("checked");
+    }
+
+    if (!(cards === null)) {
+        for (const element of cards) {
+            element.classList.remove(localStorage.getItem("old-card"));
+            element.classList.add(localStorage.getItem("new-card"));
+        }
+    }
+
+    if (!(tableHeads === null)) {
+        for (const element of tableHeads) {
+            element.classList.remove(localStorage.getItem("old-card"));
+            element.classList.add(localStorage.getItem("new-card"));
+        }
+    }
+
+    if (!(formControls === null)) {
+        for (const element of formControls) {
+            element.classList.remove(localStorage.getItem("old-card"));
+            element.classList.add(localStorage.getItem("new-card"));
+        }
+    }
+
+    if (!(userTable === null)) {
+        userTable.className = localStorage.getItem('table') || 'table table-striped table-hover';
+        categoryTable.className = localStorage.getItem('table') || 'table table-striped table-hover';
+        countryTable.className = localStorage.getItem('table') || 'table table-striped table-hover';
+        companyTable.className = localStorage.getItem('table') || 'table table-striped table-hover';
     }
 }
 
