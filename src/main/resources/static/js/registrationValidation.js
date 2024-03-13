420c28f9e22db182f8d0d7c580a8fa2b00aca17b
Massonus <maksym.lithuanian@gmail.com>
2024-03-13 16:06:21 +0200

    fix registration bag

--- src/main/resources/static/js/registrationValidation.js
+++ src/main/resources/static/js/registrationValidation.js
@@ -9,39 +9,39 @@ function validateForm(event) {
     if (username === "") {
         document.getElementById("usernameError").textContent = "Please input username";
         document.getElementById("usernameAlert").classList.remove('d-none');
-        return;
+        return false;
 
     } else if (!/^[a-zA-Z][a-zA-Z0-9_]*$/.test(username)) {
         document.getElementById("usernameError").textContent = "Username must start with a letter and contain only letters, numbers, and underscores";
         document.getElementById("usernameAlert").classList.remove('d-none');
-        return;
+        return false;
     }
 
     if (password === "") {
         document.getElementById("passwordError").textContent = "Please input password";
         document.getElementById("passwordAlert").classList.remove('d-none');
-        return;
+        return false;
 
     } else if (!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).{4,15}$/.test(password)) {
         document.getElementById("passwordError").textContent = "Password must be 4-15 characters long and contain at least one letter, one digit, and one special character";
         document.getElementById("passwordAlert").classList.remove('d-none');
-        return;
+        return false;
 
     } else if (password !== confirmPassword) {
         document.getElementById("passwordError").textContent = "Passwords are different";
         document.getElementById("passwordAlert").classList.remove('d-none');
-        return;
+        return false;
     }
 
     if (email === "") {
         document.getElementById("emailError").textContent = "Please input email";
         document.getElementById("emailAlert").classList.remove('d-none');
-        return;
+        return false;
 
     } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
         document.getElementById("emailError").textContent = "Invalid email format";
         document.getElementById("emailAlert").classList.remove('d-none');
-        return;
+        return false;
     }
 
     regUser(event, username, email, password, csrf);
