15957a0fcaeb59801c79410ec16dd682094b22f9
Massonus <maksym.lithuanian@gmail.com>
2024-03-14 19:17:56 +0200

    edit styles

--- src/main/java/com/massonus/rccnavigator/controller/ProductController.java
+++ src/main/java/com/massonus/rccnavigator/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.massonus.rccnavigator.controller;
 import com.massonus.rccnavigator.dto.ProductDto;
 import com.massonus.rccnavigator.entity.Product;
 import com.massonus.rccnavigator.entity.ProductCategory;
-import com.massonus.rccnavigator.service.BasketObjectService;
-import com.massonus.rccnavigator.service.BasketService;
-import com.massonus.rccnavigator.service.ProductService;
-import com.massonus.rccnavigator.service.WishService;
+import com.massonus.rccnavigator.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -26,13 +23,15 @@ public class ProductController {
     private final BasketObjectService basketObjectService;
     private final BasketService basketService;
     private final WishService wishService;
+    private final CompanyService companyService;
 
     @Autowired
-    public ProductController(ProductService productService, BasketObjectService basketObjectService, BasketService basketService, WishService wishService) {
+    public ProductController(ProductService productService, BasketObjectService basketObjectService, BasketService basketService, WishService wishService, CompanyService companyService) {
         this.productService = productService;
         this.basketObjectService = basketObjectService;
         this.basketService = basketService;
         this.wishService = wishService;
+        this.companyService = companyService;
     }
 
     @GetMapping("/all-products")
@@ -64,7 +63,7 @@ public class ProductController {
     public String getProductsOfCompanyForAdmin(@RequestParam Long companyId, Model model) {
         List<Product> products = productService.getAllProductsByCompanyId(companyId);
         model.addAttribute("products", products);
-        model.addAttribute("companyId", companyId);
+        model.addAttribute("company", companyService.getCompanyById(companyId));
 
         return "product/productsInAdminPanel";
     }
@@ -75,7 +74,7 @@ public class ProductController {
 
         model.addAttribute("companyId", companyId);
 
-        return "product/addProductToCompany";
+        return "product/addProduct";
 
     }
 
