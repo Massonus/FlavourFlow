e4e95f5b23535f34059f3a66da399fb63fd2231f
Massonus <maksym.lithuanian@gmail.com>
2024-04-12 21:05:53 +0300

    refactor

--- src/main/java/com/massonus/rccnavigator/service/ImageService.java
+++ src/main/java/com/massonus/rccnavigator/service/ImageService.java
@@ -16,7 +16,7 @@ import java.io.InputStream;
 @Service
 public class ImageService {
 
-    private static final String ACCESS_TOKEN = "sl.BzNMiTsyANfrb7zaPDb_cykgXRYJSTgqS02WOoBxixXIniZs0Fpgv3Cr7MOia0BCYqPtbRsRFs79zdKbnjrSjFyyfFgSVNSjwKGuoxKXyCALKAqYlmM2zxW2IbJ94CcXAGMyW93mGIwBLhL-gWFY";
+    private static final String ACCESS_TOKEN = "sl.BzN4zPKwWTp4mhZUeScmIMH3vkFuQpLfamZKZhP3HeazwHGPJzxjxlelq2bULKMRFHdp3RrGoPopglNHnmZBHA7f-ACuVlJH420LqIUos-FN7VlFS7nIJ_6MYQU56VJm5_gVDvO6lO3Cv9OWn3aj";
 
     public ImageResponseDto upload(final MultipartFile file) {
 
