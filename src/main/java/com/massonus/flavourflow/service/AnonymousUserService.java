561a0e25d9d668ec5ec4931dfbcd33eee991de60
Massonus <maksym.lithuanian@gmail.com>
2024-04-21 19:05:57 +0300

    added anonymous user

---
+++ src/main/java/com/massonus/flavourflow/service/AnonymousUserService.java
@@ -0,0 +1,36 @@
+package com.massonus.flavourflow.service;
+
+import com.massonus.flavourflow.entity.AnonymousUser;
+import com.massonus.flavourflow.repo.AnonymousUserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class AnonymousUserService {
+
+    private final AnonymousUserRepo anonymousRepo;
+
+    @Autowired
+    public AnonymousUserService(AnonymousUserRepo anonymousRepo) {
+        this.anonymousRepo = anonymousRepo;
+    }
+
+    public AnonymousUser getAnonymousById(final Long id) {
+        return anonymousRepo.findAnonymousUserById(id);
+    }
+
+    public AnonymousUser getAnonymousByIp(final String ip) {
+        return anonymousRepo.findAnonymousUserByIp(ip);
+    }
+
+    public void saveAnonymous(final String ip) {
+        if (Objects.nonNull(getAnonymousByIp(ip))) {
+            return;
+        }
+        AnonymousUser anonymous = new AnonymousUser();
+        anonymous.setIp(ip);
+        anonymousRepo.save(anonymous);
+    }
+}
