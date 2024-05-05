75d5da83672635cc81ca726f2f7663df01fd0331
Massonus <maksym.lithuanian@gmail.com>
2024-05-05 15:20:12 +0300

    refactor wish heart

--- src/main/resources/static/js/wishes.js
+++ src/main/resources/static/js/wishes.js
@@ -19,6 +19,7 @@ function saveItem(productId, csrf, iconElement) {
         .then(response => {
             if (response.ok) {
                 iconElement.className = "bi bi-heart-fill";
+                checkWishes();
             } else {
                 alert("Error! Reload the page and try again");
             }
@@ -45,10 +46,12 @@ function deleteWishItem(productId, csrf, iconElement) {
 
             if (iconElement !== undefined) {
                 iconElement.className = "bi bi-heart";
+                checkWishes();
 
             } else if (data !== undefined) {
                 console.log(data.itemId);
                 document.getElementById(`wish-item-${data.itemId}`).remove();
+                checkWishes();
             } else {
                 alert("Error! Reload the page and try again");
             }
@@ -81,6 +84,7 @@ function moveWishToBasket(productId, csrf) {
             } else {
                 document.getElementById(`wish-item-${data.itemId}`).remove();
                 changeBasketObjectsCount();
+                checkWishes();
             }
 
         })
@@ -124,6 +128,35 @@ function openAlertWindow() {
         if (event._isClickWithInModal) return;
         event.currentTarget.classList.remove('open');
     });
+}
+
+function checkWishes() {
+
+    const url = `/wishes/check`;
+
+    fetch(url, {
+        method: 'GET',
+    })
+        .then(res => res.json())
+        .then((data) => {
+            if (!data) {
+                document.getElementById("wish-icon").innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-heart-fill\" viewBox=\"0 0 16 16\">\n" +
+                    "<path fill-rule=\"evenodd\" d=\"M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314\"/>\n" +
+                    "</svg>";
+
+            } else {
+                document.getElementById("wish-icon").innerHTML = "<svg id=\"wish-icon\" xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-heart\" viewBox=\"0 0 16 16\">\n" +
+                    "                                <path d=\"m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15\"/>\n" +
+                    "                            </svg>";
+
+                let button = document.getElementById("clear-wish-button");
+                if (button !== null) {
+                    button.remove();
+                }
+            }
+
+        })
+        .catch(error => console.error(error));
 }
 
 function closeWishAlertWindow() {
